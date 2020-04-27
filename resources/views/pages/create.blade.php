c7e83113482de5fa78e825066828ebfa8a5d0b85
Ji20on <lmarkjason.20@gmail.com>
2020-04-28 07:03:31 +0800

    input added name tag

--- resources/views/pages/create.blade.php
+++ resources/views/pages/create.blade.php
@@ -33,30 +33,30 @@
                                     <div class="col-12 col-sm-6 col-md-4">
                                         <div class="form-group">
                                             <label class="font-weight-bold" for="startDate">Start Date:</label>
-                                            <input type="date" class="form-control" id="startDate" aria-describedby="startDate">
+                                            <input type="date" class="form-control" name="startDate" id="startDate" aria-describedby="startDate">
                                         </div>
                 
                                         <div class="form-group">
                                             <label class="font-weight-bold" for="stopDate">Stop Date:</label>
-                                            <input type="date" class="form-control" id="stopDate" aria-describedby="stopDate">
+                                            <input type="date" class="form-control" name="stopDate" id="stopDate" aria-describedby="stopDate">
                                         </div>
                                     </div>
 
                                     <div class="col-12 col-sm-6 col-md-4">
                                         <div class="form-group">
                                             <label class="font-weight-bold" for="periodType">Period Type : </label>
-                                            <input type="text" class="form-control" id="periodType" aria-describedby="periodType">
+                                            <input type="text" class="form-control" name="periodType" id="periodType" aria-describedby="periodType">
                                         </div>
                                         <div class="form-group">
                                             <label class="font-weight-bold" for="periodDay">Period Day : </label>
-                                            <input type="text" class="form-control" id="periodDay" aria-describedby="periodDay">
+                                            <input type="text" class="form-control" name="periodDay" id="periodDay" aria-describedby="periodDay">
                                         </div>
                                     </div>
 
                                     <div class="col-12 col-sm-6 col-md-4">
                                         <div class="form-group">
                                             <label class="font-weight-bold" for="nextShipDate">Next Ship Date:</label>
-                                            <input type="date" class="form-control" id="nextShipDate" aria-describedby="nextShipDate">
+                                            <input type="date" class="form-control" name="nextShipDate" id="nextShipDate" aria-describedby="nextShipDate">
                                         </div>
                                     </div>
 
@@ -75,22 +75,22 @@
                                     <div class="col-12 col-sm-6 col-md-4">
                                         <div class="form-group">
                                             <label class="font-weight-bold" for="shipName">Ship Name : </label>
-                                            <input type="text" class="form-control" id="shipName" aria-describedby="shipName">
+                                            <input type="text" class="form-control" name="shipName" id="shipName" aria-describedby="shipName">
                                         </div>
                 
                                         <div class="form-group">
                                             <label class="font-weight-bold" for="shipStreet1">Ship Street 1 : </label>
-                                            <input type="text" class="form-control" id="shipStreet1" aria-describedby="shipStreet1">
+                                            <input type="text" class="form-control" name="shipStreet1" id="shipStreet1" aria-describedby="shipStreet1">
                                         </div>
                 
                                         <div class="form-group">
                                             <label class="font-weight-bold" for="shipStreet2">Ship Street 2 : </label>
-                                            <input type="text" class="form-control" id="shipStreet2" aria-describedby="shipStreet2">
+                                            <input type="text" class="form-control" name="shipStreet2" id="shipStreet2" aria-describedby="shipStreet2">
                                         </div>
 
                                         <div class="form-group">
                                             <label class="font-weight-bold"  for="shipCity">Ship City : </label>
-                                            <select class="custom-select" id="shipCity">
+                                            <select class="custom-select" name="shipCity" id="shipCity">
                                                 <option selected>Choose...</option>
                                                 <option value="1">LA MIRADA</option>
                                                 <option value="2">LAMIRADA</option>
@@ -100,7 +100,7 @@
 
                                         <div class="form-group">
                                             <label class="font-weight-bold"  for="shipState">Ship State : </label>
-                                            <select class="custom-select" id="shipState">
+                                            <select class="custom-select" name="shipState" id="shipState">
                                                 <option selected>Choose...</option>
                                                 <option value="1">CA</option>
                                               </select>
@@ -111,12 +111,12 @@
                                     <div class="col-12 col-sm-6 col-md-4">                
                                         <div class="form-group">
                                             <label class="font-weight-bold" for="shipPostalCode">Ship Postal Code : </label>
-                                            <input type="text" class="form-control" id="shipPostalCode" aria-describedby="shipPostalCode">
+                                            <input type="text" class="form-control" name="shipPostalCode" id="shipPostalCode" aria-describedby="shipPostalCode">
                                         </div>
 
                                         <div class="form-group">
                                             <label class="font-weight-bold"  for="shipCounty">Ship County : </label>
-                                            <select class="custom-select" id="shipCounty">
+                                            <select class="custom-select" name="shipCounty" id="shipCounty">
                                                 <option selected>Choose...</option>
                                                 <option value="1">Los Angeles</option>
                                               </select>
@@ -124,7 +124,7 @@
 
                                         <div class="form-group">
                                             <label class="font-weight-bold"  for="shipCountry">Ship Country : </label>
-                                            <select class="custom-select" id="shipCountry">
+                                            <select class="custom-select" name="shipCountry" id="shipCountry">
                                                 <option selected>Choose...</option>
                                                 <option value="1">USA</option>
                                               </select>
@@ -132,7 +132,7 @@
 
                                         <div class="form-group">
                                             <label class="font-weight-bold" for="shipMethod">Ship Method : </label>
-                                            <input type="text" class="form-control" id="shipMethod" aria-describedby="shipMethod">
+                                            <input type="text" class="form-control" name="shipMethod" id="shipMethod" aria-describedby="shipMethod">
                                         </div>
                                     </div>
 
@@ -140,22 +140,22 @@
 
                                         <div class="form-group">
                                             <label class="font-weight-bold" for="shipPhone">Ship Phone : </label>
-                                            <input type="text" class="form-control" id="shipPhone" aria-describedby="shipPhone">
+                                            <input type="text" class="form-control" name="shipPhone" id="shipPhone" aria-describedby="shipPhone">
                                         </div>
 
                                         <div class="form-group">
                                             <label class="font-weight-bold" for="shipMethodID">Ship Method ID : </label>
-                                            <input type="text" class="form-control" id="shipMethodID" aria-describedby="shipMethodID">
+                                            <input type="text" class="form-control" name="shipMethodID" id="shipMethodID" aria-describedby="shipMethodID">
                                         </div>
 
                                         <div class="form-group">
                                             <label class="font-weight-bold" for="overrideShipping">Override Shipping : </label>
-                                            <input type="text" class="form-control" id="overrideShipping" aria-describedby="overrideShipping">
+                                            <input type="text" class="form-control" name="overrideShipping" id="overrideShipping" aria-describedby="overrideShipping">
                                         </div>
 
                                         <div class="form-group">
                                             <label class="font-weight-bold" for="overrideShippingTotal">Override Shipping Total : </label>
-                                            <input type="text" class="form-control" id="overrideShippingTotal" aria-describedby="overrideShippingTotal">
+                                            <input type="text" class="form-control" name="overrideShippingTotal" id="overrideShippingTotal" aria-describedby="overrideShippingTotal">
                                         </div>
 
                                     </div>
@@ -173,14 +173,18 @@
                                 <div class="row">
 
                                     <div class="col-12 col-sm-6 col-md-4">
+
                                         <div class="form-group">
-                                            <label class="font-weight-bold" for="paymentType">Payment Type : </label>
-                                            <input type="text" class="form-control" id="paymentType" aria-describedby="paymentType">
+                                            <label class="font-weight-bold"  for="paymentType">Payment Type : </label>
+                                            <select class="custom-select" name="paymentType" id="paymentType">
+                                                <option selected>Choose...</option>
+                                                <option value="1">Credit Card</option>
+                                              </select>
                                         </div>
                 
                                         <div class="form-group">
                                             <label class="font-weight-bold" for="currencyTypeID">Currency Type ID : </label>
-                                            <input type="text" class="form-control" id="currencyTypeID" aria-describedby="currencyTypeID">
+                                            <input type="text" class="form-control" name="currencyTypeID" id="currencyTypeID" aria-describedby="currencyTypeID">
                                         </div>
                                     </div>
 
